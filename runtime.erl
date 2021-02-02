fcc49ba0c0aef601005b08d116a7dd77f9443191
Louis Pilfold <louis@lpil.uk>
2021-02-02 14:34:31 +0000

    Error if the boot module is instructed to run itself

--- runtime.erl
+++ runtime.erl
@@ -6,8 +6,8 @@ main() ->
     Mod = get_main_module(),
     erlang:display({"Starting", Mod}),
     % erlang:display(code:all_loaded()),
-    erlang:display(length(processes())),
-    % length(registered()).)
+    % erlang:display(length(processes())),
+    % length(registered())).
     R = Mod:main(),
     erlang:display({"Done", R}),
     erlang:halt().
@@ -16,7 +16,12 @@ module_name(Str) ->
     case catch list_to_atom(Str) of
         {'EXIT', _} ->
             erlang:display("Bad module name: " ++ Str),
-            erlang:halt();
+            erlang:halt(1);
+
+        ?MODULE ->
+            erlang:display("Attempting to run boot module. Try another module"),
+            erlang:halt(1);
+
         Mod -> Mod
     end.
 
@@ -24,6 +29,6 @@ get_main_module() ->
     case init:get_argument(load) of
         {ok, [[Arg]]} -> module_name(Arg);
         _ ->
-            erlang:display("Missing: -load Mod"),
-            erlang:halt()
+            erlang:display("Missing command line argument: -load Mod"),
+            erlang:halt(1)
     end.
